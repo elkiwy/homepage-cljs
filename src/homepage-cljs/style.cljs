b841314f26474db4d92fec36ba910aab6c7ea912
Stefano Bertoli <elkiwydev@gmail.com>
2019-07-01 22:38:59 +0200

    added custom colors from native project

--- src/homepage-cljs/style.cljs
+++ src/homepage-cljs/style.cljs
@@ -12,6 +12,13 @@
 (def bg3 "#d0d0d0f0")
 
 
+(def col-white "#f8f4f0")
+(def col-black "#000408e0")
+(def col-black-full "#000408")
+(def col-dark-gray "#303030")
+(def col-medium-gray "#252228")
+
+
 (defstyles background []
     {:background-color bg1
      :box-shadow (str "4px 4px 16px -10px " fg1)
@@ -19,8 +26,9 @@
      :transition-duration "0.2s"
      :transition-timing-function "ease-in-out"
      :border-width "1px"
-     :border-radius "5px"
+     :border-radius "2px"
      :border-style "solid"
      :&:hover {:background-color bg2
                :box-shadow (str "8px 8px 26px -10px " fg1)
                :transform "scale(1.05)"}})
+
